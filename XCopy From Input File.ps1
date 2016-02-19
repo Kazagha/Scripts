8a691b3c7b66cd36e244301dc9f082918b4850c3
Kazagha <chrisg.local@gmail.com>
2016-02-19 11:10:33 +1000

    XCopy From Input File: Simpify
    Remove the split between directories and files, the script will copy the contents of any directories in the source list.

--- XCopy From Input File.ps1
+++ XCopy From Input File.ps1
@@ -1,26 +1,18 @@
+
 # Source File: List of directory and filenames, new line for each
-$sourceFile = "C:\temp\search\results.txt"
+$sourceFile = "C:\temp\search\S Drive results.txt"
 # Destinaton: The destination directory /w blackslash at the end
 $dest = "C:\temp\Search\"
 
 Get-Content $sourceFile | ForEach-Object {    
 
+    # Grab the item in the source file
     $item = Get-Item $_.ToString()
+
+    # Determine destination directory including the full source path 
+    $destDir = $dest + $_.Substring(2, $_.LastIndexOf("\") - 1)
+
+    # Copy the item into the destination directory
+    XCOPY /Y /S /I $item $destDir        
     
-    IF ( $item -is [System.IO.DirectoryInfo] )
-    {
-        # Copy Directories - Not required at the moment
-
-        # Write-Output  ("Copying Directory: {0}" -f $item)               
-    } ELSE {
-        # Copy files
-
-        # Write-Output ("Copying File {0}" -f $item)
-        
-        # Determine destination directory including the full source path 
-        $destDir = $dest + $_.Substring(2, $_.LastIndexOf("\") - 1)
-
-        # Copy the file to the destination directory 
-        XCOPY /Y /I $item $destDir        
-    }
 }
